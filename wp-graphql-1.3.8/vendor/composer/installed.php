b9a4a6c1c9f211330f025c147467fe8b58aeb6db
chriszarate <chris.zarate@automattic.com>
2021-05-20 18:44:40 -0400

    Add WPGraphQL 1.3.8

---
+++ wp-graphql-1.3.8/vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'v1.3.8',
+    'version' => '1.3.8.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f0cddfea89edd0d2ef344220523fe391c6ac206c',
+    'name' => 'wp-graphql/wp-graphql',
+  ),
+  'versions' => 
+  array (
+    'ivome/graphql-relay-php' => 
+    array (
+      'pretty_version' => 'v0.5.0',
+      'version' => '0.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b227c17e79564d147db2162329f5bdbe19192313',
+    ),
+    'webonyx/graphql-php' => 
+    array (
+      'pretty_version' => 'v14.4.0',
+      'version' => '14.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aab3d49181467db064b41429cde117a7589625fc',
+    ),
+    'wp-graphql/wp-graphql' => 
+    array (
+      'pretty_version' => 'v1.3.8',
+      'version' => '1.3.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f0cddfea89edd0d2ef344220523fe391c6ac206c',
+    ),
+  ),
+);
