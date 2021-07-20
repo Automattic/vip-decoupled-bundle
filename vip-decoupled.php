5df826d52e16a7594d7a57c612d7558de97c58ed
chriszarate <chris.zarate@automattic.com>
2021-07-20 19:55:19 -0400

    Upgrade WPGraphQL to 1.5.4

--- vip-decoupled.php
+++ vip-decoupled.php
@@ -23,10 +23,10 @@ use function WPCOMVIP\Decoupled\Settings\is_plugin_enabled;
 require_once __DIR__ . '/settings/settings.php';
 
 /**
- * WPGraphQL 1.4.3
+ * WPGraphQL 1.5.4
  */
 if ( is_plugin_enabled( 'plugin_wpgraphql' ) ) {
-	require_once __DIR__ . '/wp-graphql-1.4.3/wp-graphql.php';
+	require_once __DIR__ . '/lib/wp-graphql-1.5.4/wp-graphql.php';
 }
 
 /**
