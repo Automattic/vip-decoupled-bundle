924cb187adb71f1b816c55b84ea09f408da83113
chriszarate <chris.zarate@automattic.com>
2021-07-20 21:58:59 -0400

    Add PHPUnit tests

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Vip_Decoupled_Bundle
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
