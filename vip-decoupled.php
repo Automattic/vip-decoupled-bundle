d7f12250528d6eb59ee532384860e885e2d78771
ingeniumed <ingeniumed@users.noreply.github.com>
2023-10-20 15:19:02 +1100

    Update the packages in composer.json

--- vip-decoupled.php
+++ vip-decoupled.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Plugin Name: VIP Decoupled Plugin Bundle
- * Plugin URI: https://wpvip.com
+ * Plugin URI: https://github.com/Automattic/vip-decoupled-bundle
  * Description: Plugin bundle to quickly provide a decoupled WordPress setup.
  * Author: WordPress VIP
  * Text Domain: vip-decoupled-bundle
- * Version: 0.1.0
- * Requires at least: 5.6.0
- * Tested up to: 5.7.1
- * Requires PHP: 7.2
+ * Version: 1.0.0
+ * Requires at least: 5.9.0
+ * Tested up to: 6.3.0
+ * Requires PHP: 7.4
  * License: GPL-3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  *
